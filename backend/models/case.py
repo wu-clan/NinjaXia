b037eb73f214826a6639e574c353c091e44f1dbc
wu <jianhengwu0407@gmail.com>
2021-12-27 16:48:06 +0800

    更新模型

--- backend/models/case.py
+++ backend/models/case.py
@@ -9,20 +9,20 @@ class Case(models.Model):
     """
     用例表
     """
-    name = models.CharField(max_length=128, unique=True, verbose_name='用例名称')
-    description = models.TextField(blank=True, null=True, verbose_name='用例描述')
-    header = models.TextField(blank=True, null=True, verbose_name='请求头')
     TYPE = (
         (0, 'params'),
         (1, 'json'),
     )
-    body_type = models.SmallIntegerField(choices=TYPE, default=0, verbose_name='请求参数类型')
-    body = models.TextField(blank=True, null=True, verbose_name='请求参数')
-    response = models.JSONField(blank=True, null=True, default=dict, verbose_name='响应data')
     RUN_STATUS = (
         (0, '失败'),
         (1, '通过'),
     )
+    name = models.CharField(max_length=128, unique=True, verbose_name='用例名称')
+    description = models.TextField(blank=True, null=True, verbose_name='用例描述')
+    header = models.TextField(blank=True, null=True, verbose_name='请求头')
+    body_type = models.SmallIntegerField(choices=TYPE, default=0, verbose_name='请求参数类型')
+    body = models.TextField(blank=True, null=True, verbose_name='请求参数')
+    response = models.JSONField(blank=True, null=True, default=dict, verbose_name='响应data')
     result = models.SmallIntegerField(choices=RUN_STATUS, default=0, verbose_name='执行结果')
     interface_id = models.ForeignKey(Interface, verbose_name='所属接口', on_delete=models.CASCADE)
 
--- backend/models/interface.py
+++ backend/models/interface.py
@@ -9,14 +9,14 @@ class Interface(models.Model):
     """
     接口表
     """
-    name = models.CharField(max_length=32, verbose_name='接口名称')
-    description = models.TextField(blank=True, null=True, verbose_name='接口描述')
     REQUEST_TYPE = (
         (0, 'GET'),
         (1, 'POST'),
         (2, 'PUT'),
         (3, 'DELETE'),
     )
+    name = models.CharField(max_length=32, verbose_name='接口名称')
+    description = models.TextField(blank=True, null=True, verbose_name='接口描述')
     method = models.SmallIntegerField(choices=REQUEST_TYPE, default=0, verbose_name='请求类型')
     url = models.CharField(max_length=255, verbose_name='请求url')
     project_id = models.ForeignKey(Project, verbose_name='所属项目', on_delete=models.CASCADE)
