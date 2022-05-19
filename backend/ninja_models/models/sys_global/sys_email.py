e94acbfb7a53ce7209fc985791e6d13d9da152da
wu <jianhengwu0407@gmail.com>
2022-05-19 18:45:33 +0800

    ✨ Add: 添加系统邮箱配置model

---
+++ backend/ninja_models/models/sys_global/sys_email.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from django.db import models
+
+
+class Sender(models.Model):
+    """
+    发件人
+    """
+    name = models.CharField(max_length=128, verbose_name='发件人名称')
+    email = models.EmailField(verbose_name='发件人邮箱')
+    password = models.CharField(max_length=128, verbose_name='发件人邮箱密码')
+    smtp_server = models.CharField(max_length=32, verbose_name='发件服务器')
+    smtp_port = models.IntegerField(verbose_name='发件服务器端口')
+    is_ssl = models.BooleanField(default=False, verbose_name='是否使用SSL')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'sys_email_sender'
+
+
+class ReceiverGroup(models.Model):
+    """
+    收件人组
+    """
+    name = models.CharField(max_length=128, verbose_name='收件人组名称')
+    description = models.TextField(null=True, verbose_name='收件人组描述')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'sys_email_receiver_group'
+
+
+class Receiver(models.Model):
+    """
+    收件人
+    """
+    name = models.CharField(max_length=128, verbose_name='收件人名称')
+    email = models.EmailField(verbose_name='收件人邮箱')
+    receiver_group = models.ForeignKey(ReceiverGroup, on_delete=models.CASCADE, verbose_name='所属收件人组',
+                                       related_name='receiver', related_query_name='receiver')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'sys_email_receiver'
