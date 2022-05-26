24948c052c89982f1c35b55ef28c359d37ff320c
wu <jianhengwu0407@gmail.com>
2022-05-26 23:43:12 +0800

    ✨ Feat: 对api_task添加retry预期项

--- backend/ninja_models/models/api_test/api_test_task.py
+++ backend/ninja_models/models/api_test/api_test_task.py
@@ -25,6 +25,7 @@ class ApiTestTask(BaseModel):
     status = models.BooleanField(default=1, verbose_name='任务状态')
     state = models.CharField(max_length=32, verbose_name='任务运行状态')
     execute_target = models.SmallIntegerField(default=0, verbose_name='执行目标, 0: 业务, 1: 用例')
+    retry = models.IntegerField(default=0, verbose_name='重试次数')
     api_case = models.CharField(max_length=256, null=True, verbose_name='拥有测试用例')
     api_project = models.ForeignKey(ApiTestProject, on_delete=models.SET_NULL, null=True, verbose_name='所属项目',
                                     related_name='api_test_tasks', related_query_name='api_test_task')
