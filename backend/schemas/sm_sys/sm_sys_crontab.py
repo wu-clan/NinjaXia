af1f30795224a0a37c13b1237df52188bc03dd17
wu <jianhengwu0407@gmail.com>
2022-05-29 06:22:47 +0800

    ✨ Add: 提交sys_crontab相关接口

---
+++ backend/schemas/sm_sys/sm_sys_crontab.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import datetime
+
+from ninja import Schema
+from pydantic import Field, validator
+
+
+class CornTabBase(Schema):
+    second: str = Field(..., regex=r'^(\*|(?:[0-9]$|(?:[1-5][0-9]$)))')
+    minute: str = Field(..., regex=r'^(\*|(?:[0-9]$|(?:[1-5][0-9]$)))')
+    hour: str = Field(..., regex=r'^(\*|(?:[0-9]$|1[0-9]$|2[0-3]$))')
+    day: str = Field(..., regex=r'^(\*|(?:[1-9]$|(?:[12][0-9])$|3[01]$))')
+    month: str = Field(..., regex=r'^(\*|(?:[1-9]$|1[012]$))')
+    day_of_week: str = Field(..., regex=r'^(\*|(?:[0-6]$))')
+
+
+class CreateCornTab(CornTabBase):
+    pass
+
+
+class UpdateCornTab(CornTabBase):
+    pass
+
+
+class GetAllCornTabs(CornTabBase):
+    id: int
+    creator: str = None
+    modifier: str = None
+    created_time: datetime.datetime
+    modified_time: datetime.datetime
